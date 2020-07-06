ee980e39b29431dc03dcf134a546e85a01e2d0f6
taohaihui <taohaihui@78dk.com>
2020-07-06 22:29:25 +0800

    打包文件

---
+++ precache-manifest.974858a1714457ce922784d09c1a14c7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "becc0b0b0743eab5b8b0f677c43117b0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "114f41e4353f748e90ea",
+    "url": "/static/css/2.c27a359e.chunk.css"
+  },
+  {
+    "revision": "1de30a51e412d6edb08f",
+    "url": "/static/css/main.f4ca60c9.chunk.css"
+  },
+  {
+    "revision": "114f41e4353f748e90ea",
+    "url": "/static/js/2.53d44a38.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.53d44a38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1de30a51e412d6edb08f",
+    "url": "/static/js/main.7ffaedf6.chunk.js"
+  },
+  {
+    "revision": "e9ba2e46f58b24551675",
+    "url": "/static/js/runtime-main.74321770.js"
+  }
+]);
